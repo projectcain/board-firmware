ac4a9180062fb5b602dceaa9798348d058cf2dae
projectcain <71684775+projectcain@users.noreply.github.com>
2023-04-06 16:09:05 -0500

    Add files via upload

---
+++ vault35/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x48, 0x77, 0xFD, 0x92, 0x08, 0xA5, 0x3B, 0x19}
+#define ENCODERS_PAD_A { D7 }
+#define ENCODERS_PAD_B { D6 }
+#define RGB_DI_PIN D5
+#define RGBLED_NUM 12 // Number of LEDs
+#define RGBLIGHT_ANIMATIONS
