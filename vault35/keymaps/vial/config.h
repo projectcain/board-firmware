351f59f35486f1fa760a5d8a8411adbd231ca4f1
projectcain <71684775+projectcain@users.noreply.github.com>
2023-04-06 16:06:22 -0500

    Delete vault35 directory

--- vault35/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x48, 0x77, 0xFD, 0x92, 0x08, 0xA5, 0x3B, 0x19}
-#define ENCODERS_PAD_A { D7 }
-#define ENCODERS_PAD_B { D6 }
