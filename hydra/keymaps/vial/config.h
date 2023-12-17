6da4b82dccf719ba2bdcb58d0e14f2cc36b798d2
projectcain <71684775+projectcain@users.noreply.github.com>
2023-12-17 09:51:03 -0600

    Delete hydra directory

--- hydra/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0xF4, 0x34, 0x02, 0xAF, 0xF5, 0x77, 0x8C, 0xA6}
-#define ENCODERS_PAD_A { D6, D1, D5 }
-#define ENCODERS_PAD_B { D4, D2, E6 }
