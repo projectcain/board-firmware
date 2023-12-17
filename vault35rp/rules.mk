e5feb35546b0de9fd9b4c3e63347285c53cab462
projectcain <71684775+projectcain@users.noreply.github.com>
2023-12-17 09:56:48 -0600

    Add files via upload

---
+++ vault35rp/rules.mk
@@ -0,0 +1,16 @@
+# MCU name
+MCU = RP2040
+BOOTLOADER = rp2040
+
+# Build Options
+#   change yes to no to disable
+#
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
+#RP2040 Options
+ALLOW_WARNINGS = yes
