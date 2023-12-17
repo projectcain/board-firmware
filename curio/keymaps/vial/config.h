8964b02a13ecba42c19587ceb09f4702a6970226
projectcain <71684775+projectcain@users.noreply.github.com>
2023-12-17 09:49:27 -0600

    Add files via upload

---
+++ curio/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1A, 0x42, 0xD5, 0xFD, 0x12, 0x4A, 0x10, 0x0D}
+#define VIAL_COMBO_ENTRIES 15
+#define VIAL_TAP_DANCE_ENTRIES 15
